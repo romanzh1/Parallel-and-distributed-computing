87894e62811b9bc656997a4504145c589b82fccc
romanzh1 <61348803+romanzh1@users.noreply.github.com>
2021-09-24 13:34:34 +0300

    feat: add matrix multiplication program

---
+++ Parallel-matrix-multip/Parallel-matrix-multip/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Parallel_matrix_multip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Okno1 = new System.Windows.Forms.TextBox();
+            this.Str1 = new System.Windows.Forms.Label();
+            this.Okno3 = new System.Windows.Forms.TextBox();
+            this.Str5 = new System.Windows.Forms.Label();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Str4 = new System.Windows.Forms.Label();
+            this.Okno4 = new System.Windows.Forms.TextBox();
+            this.Str7 = new System.Windows.Forms.Label();
+            this.Okno5 = new System.Windows.Forms.TextBox();
+            this.Okno7 = new System.Windows.Forms.TextBox();
+            this.Str8 = new System.Windows.Forms.Label();
+            this.Str10 = new System.Windows.Forms.Label();
+            this.Str11 = new System.Windows.Forms.Label();
+            this.Str12 = new System.Windows.Forms.Label();
+            this.Str13 = new System.Windows.Forms.Label();
+            this.Str14 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Button1.Location = new System.Drawing.Point(635, 32);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(120, 52);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Gen";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Okno1
+            // 
+            this.Okno1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Okno1.Location = new System.Drawing.Point(160, 32);
+            this.Okno1.Multiline = true;
+            this.Okno1.Name = "Okno1";
+            this.Okno1.Size = new System.Drawing.Size(77, 52);
+            this.Okno1.TabIndex = 1;
+            this.Okno1.Text = "3";
+            this.Okno1.TextChanged += new System.EventHandler(this.Okno1_TextChanged);
+            // 
+            // Str1
+            // 
+            this.Str1.AutoSize = true;
+            this.Str1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str1.Location = new System.Drawing.Point(47, 47);
+            this.Str1.Name = "Str1";
+            this.Str1.Size = new System.Drawing.Size(107, 25);
+            this.Str1.TabIndex = 2;
+            this.Str1.Text = "Введите 1";
+            this.Str1.Click += new System.EventHandler(this.Str1_Click);
+            // 
+            // Okno3
+            // 
+            this.Okno3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Okno3.Location = new System.Drawing.Point(170, 117);
+            this.Okno3.Multiline = true;
+            this.Okno3.Name = "Okno3";
+            this.Okno3.Size = new System.Drawing.Size(209, 166);
+            this.Okno3.TabIndex = 6;
+            this.Okno3.TextChanged += new System.EventHandler(this.Okno3_TextChanged);
+            // 
+            // Str5
+            // 
+            this.Str5.AutoSize = true;
+            this.Str5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str5.Location = new System.Drawing.Point(54, 180);
+            this.Str5.Name = "Str5";
+            this.Str5.Size = new System.Drawing.Size(110, 25);
+            this.Str5.TabIndex = 7;
+            this.Str5.Text = "Матрица 1";
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Button2.Location = new System.Drawing.Point(501, 310);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(111, 52);
+            this.Button2.TabIndex = 10;
+            this.Button2.Text = "Multi";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Str4
+            // 
+            this.Str4.AutoSize = true;
+            this.Str4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str4.Location = new System.Drawing.Point(385, 189);
+            this.Str4.Name = "Str4";
+            this.Str4.Size = new System.Drawing.Size(110, 25);
+            this.Str4.TabIndex = 11;
+            this.Str4.Text = "Матрица 2";
+            // 
+            // Okno4
+            // 
+            this.Okno4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Okno4.Location = new System.Drawing.Point(501, 117);
+            this.Okno4.Multiline = true;
+            this.Okno4.Name = "Okno4";
+            this.Okno4.Size = new System.Drawing.Size(209, 166);
+            this.Okno4.TabIndex = 12;
+            // 
+            // Str7
+            // 
+            this.Str7.AutoSize = true;
+            this.Str7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str7.Location = new System.Drawing.Point(322, 48);
+            this.Str7.Name = "Str7";
+            this.Str7.Size = new System.Drawing.Size(107, 25);
+            this.Str7.TabIndex = 14;
+            this.Str7.Text = "Введите 2";
+            // 
+            // Okno5
+            // 
+            this.Okno5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Okno5.Location = new System.Drawing.Point(435, 33);
+            this.Okno5.Multiline = true;
+            this.Okno5.Name = "Okno5";
+            this.Okno5.Size = new System.Drawing.Size(81, 52);
+            this.Okno5.TabIndex = 13;
+            this.Okno5.Text = "3";
+            // 
+            // Okno7
+            // 
+            this.Okno7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Okno7.Location = new System.Drawing.Point(245, 443);
+            this.Okno7.Multiline = true;
+            this.Okno7.Name = "Okno7";
+            this.Okno7.Size = new System.Drawing.Size(221, 186);
+            this.Okno7.TabIndex = 17;
+            this.Okno7.Visible = false;
+            this.Okno7.TextChanged += new System.EventHandler(this.Okno7_TextChanged);
+            // 
+            // Str8
+            // 
+            this.Str8.AutoSize = true;
+            this.Str8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str8.Location = new System.Drawing.Point(11, 519);
+            this.Str8.Name = "Str8";
+            this.Str8.Size = new System.Drawing.Size(226, 25);
+            this.Str8.TabIndex = 18;
+            this.Str8.Text = "Умножение на число 1";
+            this.Str8.Visible = false;
+            this.Str8.Click += new System.EventHandler(this.Str8_Click);
+            // 
+            // Str10
+            // 
+            this.Str10.AutoSize = true;
+            this.Str10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str10.Location = new System.Drawing.Point(12, 324);
+            this.Str10.Name = "Str10";
+            this.Str10.Size = new System.Drawing.Size(290, 25);
+            this.Str10.TabIndex = 22;
+            this.Str10.Text = "Сумма элементов диагонали";
+            this.Str10.Visible = false;
+            // 
+            // Str11
+            // 
+            this.Str11.AutoSize = true;
+            this.Str11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str11.Location = new System.Drawing.Point(53, 519);
+            this.Str11.Name = "Str11";
+            this.Str11.Size = new System.Drawing.Size(175, 25);
+            this.Str11.TabIndex = 23;
+            this.Str11.Text = "Сложение 1 + 2 =";
+            this.Str11.Visible = false;
+            // 
+            // Str12
+            // 
+            this.Str12.AutoSize = true;
+            this.Str12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str12.Location = new System.Drawing.Point(53, 519);
+            this.Str12.Name = "Str12";
+            this.Str12.Size = new System.Drawing.Size(176, 25);
+            this.Str12.TabIndex = 24;
+            this.Str12.Text = "Вычитание 1 - 2 =";
+            this.Str12.Visible = false;
+            // 
+            // Str13
+            // 
+            this.Str13.AutoSize = true;
+            this.Str13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str13.Location = new System.Drawing.Point(4, 519);
+            this.Str13.Name = "Str13";
+            this.Str13.Size = new System.Drawing.Size(233, 25);
+            this.Str13.TabIndex = 25;
+            this.Str13.Text = "Транспонирование 1 и 2";
+            this.Str13.Visible = false;
+            this.Str13.Click += new System.EventHandler(this.Str13_Click);
+            // 
+            // Str14
+            // 
+            this.Str14.AutoSize = true;
+            this.Str14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Str14.Location = new System.Drawing.Point(78, 519);
+            this.Str14.Name = "Str14";
+            this.Str14.Size = new System.Drawing.Size(159, 25);
+            this.Str14.TabIndex = 26;
+            this.Str14.Text = "Перемножение";
+            this.Str14.Visible = false;
+            this.Str14.Click += new System.EventHandler(this.Str14_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox1.Location = new System.Drawing.Point(308, 310);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 52);
+            this.textBox1.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.textBox2.Location = new System.Drawing.Point(308, 377);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 52);
+            this.textBox2.TabIndex = 29;
+            this.textBox2.Text = "ms";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 25);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Время перемножения";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 640);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Str14);
+            this.Controls.Add(this.Str13);
+            this.Controls.Add(this.Str12);
+            this.Controls.Add(this.Str11);
+            this.Controls.Add(this.Str10);
+            this.Controls.Add(this.Str8);
+            this.Controls.Add(this.Okno7);
+            this.Controls.Add(this.Str7);
+            this.Controls.Add(this.Okno5);
+            this.Controls.Add(this.Okno4);
+            this.Controls.Add(this.Str4);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Str5);
+            this.Controls.Add(this.Okno3);
+            this.Controls.Add(this.Str1);
+            this.Controls.Add(this.Okno1);
+            this.Controls.Add(this.Button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "roman_zh1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.TextBox Okno1;
+        private System.Windows.Forms.Label Str1;
+        private System.Windows.Forms.TextBox Okno3;
+        private System.Windows.Forms.Label Str5;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Label Str4;
+        private System.Windows.Forms.TextBox Okno4;
+        private System.Windows.Forms.Label Str7;
+        private System.Windows.Forms.TextBox Okno5;
+        private System.Windows.Forms.TextBox Okno7;
+        private System.Windows.Forms.Label Str8;
+        private System.Windows.Forms.Label Str10;
+        private System.Windows.Forms.Label Str11;
+        private System.Windows.Forms.Label Str12;
+        private System.Windows.Forms.Label Str13;
+        private System.Windows.Forms.Label Str14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Parallel-matrix-multip/Parallel-matrix-multip/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Parallel_matrix_multip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Str1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Okno1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Str2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int N, M, Q, W;
+        double [,] a, b;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Okno7.Visible = false;
+            Str14.Visible = false;
+            Okno3.Clear();
+            Okno4.Clear();
+            N = Int32.Parse(Okno1.Text);
+            M = N;
+            Q = Int32.Parse(Okno5.Text);
+            W = Q;
+            a = new double[N, M];
+            b = new double[Q, W];
+            Random c = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++) {
+                    a[i, j] = c.NextDouble();
+                    b[i, j] = c.NextDouble();
+                }
+            }
+
+            if (N < 5)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < M; j++) Okno3.Text += (Math.Round(a[i, j], 2) + "  ");
+                    Okno3.Text += Environment.NewLine;
+                }
+                for (int i = 0; i < Q; i++)
+                {
+                    for (int j = 0; j < W; j++) Okno4.Text += (Math.Round(b[i, j], 2) + "  ");
+                    Okno4.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void Str9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Okno8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Okno7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Str8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Okno2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Str13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Spisok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Str8.Visible = false;
+            Okno7.Visible = false;
+            Str10.Visible = false;
+            Str11.Visible = false;
+            Str12.Visible = false;
+            Str13.Visible = false;
+            Str14.Visible = false;
+
+        }
+
+        private void Str14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        double[,] big;
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (N < 5) Str14.Visible = true;
+            if (N < 5) Okno7.Visible = true;
+            Okno7.Clear();
+            big = new double[a.GetLength(0), b.GetLength(1)];
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        big[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            textBox2.Text = string.Format("time: {0}", sw.ElapsedMilliseconds);
+            sw.Stop();
+
+            if (M < 5)
+            {
+                for (int i = 0; i < big.GetLength(0); i++)
+                {
+                    for (int j = 0; j < big.GetLength(1); j++)
+                    {
+                        Okno7.Text += Math.Round(big[i, j], 2) + "  ";
+                    }
+                    Okno7.Text += Environment.NewLine;
+                }
+            }
+
+            textBox1.Text = sumDiag().ToString();
+        }
+
+        private double sumDiag()
+        {
+            double sum = 0;
+            for (int j = 0; j < big.GetLength(1); j++)
+            {
+                sum += big[j, j];
+            }
+            return sum;
+        }
+
+        private void Okno3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
